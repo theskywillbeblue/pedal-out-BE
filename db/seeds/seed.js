3f12fa68eeb2dba1efe82f6e30aec29bdf6e48de
Liv Motley <livmotley.uk@gmail.com>
2025-04-14 12:24:35 +0100

    added ride and user data, as well as created tables for each

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -9,6 +9,12 @@ const seed = ({userData, ridesData, commentsData}) => {
         return db.query(`DROP TABLE IF EXISTS rides`)
     }).then(() => {
         return createUsers();
+    }).then(() => {
+        return createDisciplineType();
+    }).then(() => {
+        return createRides();
+    }).then(() => {
+        return createComments();
     })
 }
 
@@ -24,5 +30,38 @@ function createUsers() {
         )`)
 }
 
+function createDisciplineType() {
+    return db.query(`CREATE TYPE discipline_type AS ENUM
+         ('Downhill', 'Gravel', 'Mountain', 'Enduro', 'Cross-Country', 'Park', 'Casual')`)
+}
 
-module.exports = seed;
+function createRides() {
+    return db.query(`CREATE TABLE rides(
+        author REFERENCES users(username) ON DELETE CASCADE,
+        ride_id SERIAL PRIMARY KEY,
+        ride_location JSONB NOT NULL,
+        created_at TIMESTAMP,
+        ride_date DATE NOT NULL, 
+        ride_time TIME NOT NULL,
+        description VARCHAR(1000),
+        discipline discipline_type,
+        title VARCHAR(100) NOT NULL,
+        public BOOLEAN DEFAULT true,
+        participants INTEGER[] 
+        )`) 
+}
+// ^^^ participants may need to be updated to UUID[] depending on what is going to be pushed here, the ride_location 
+// datatype may need updating depending on how the map API needs to receive the data. This will also need updating in rides data (test and dev) if changed
+
+function createComments() {
+    return db.query(`CREATE TABLE comments(
+        comment_id SERIAL PRIMARY KEY,
+        ride_id INT REFERENCES rides(ride_id) ON DELETE CASCADE,
+        body TEXT NOT NULL,
+        author VARCHAR(40) REFERENCES users(username) ON DELETE CASCADE,
+        created_at TIMESTAMP
+        )`)
+}
+
+
+module.export = seed;
