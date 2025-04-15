0f14d59bcc3e6b563e4148b3cc95a2a371adc80a
Sophie King <102487877+theskywillbeblue@users.noreply.github.com>
2025-04-15 09:48:31 +0100

    app.js, api-router setup and commence rides testing

---
+++ __tests__/rides.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const seed = require('../db/seeds/seed.js');
+const data = require('../db/data/test-data/index.js');
+const db = require('../db/connection.js');
+
+beforeEach(() => {
+    return seed(data);
+})
+
+afterAll(() => {
+    return db.end();
+})
+
+describe('RIDES', () => {
+    describe('GET /api/rides', () => {
+	test('200: Get all rides', () => {
+		return request(app)
+			.get('/api/rides')
+			.expect(200)
+			.then(({ body }) => {
+                expect(body.rides).toBeInstanceOf(Array);
+                expect(body.rides.length).toBe//length;
+			});
+	});
+})
+});
--- app/app.js
+++ app/app.js
@@ -1,5 +1,35 @@
 const express = require("express");
-const app = express();
+const app = express()
+app.use(express.json());
+const cors = require ('cors')
 
 
-module.exports = app;
+// routers
+
+app.use(cors())
+
+const apiRouter = require("./api-router")
+const ridesRouter = require("./routes/rides-router")
+const commentsRouter = require("./routes/comments-router")
+const usersRouter = require("./routes/users-router");
+
+
+// endpoints
+
+app.use("/api", apiRouter)
+
+app.use("/api/rides", ridesRouter)
+
+app.use("/api/comments", commentsRouter)
+
+app.use("/api/users", usersRouter)
+
+
+
+// error handling
+
+
+
+
+
+module.exports = app
