1cb7b8ada2fc66eb9c86ca0051e6bee77b07ccf3
Liv Motley <livmotley.uk@gmail.com>
2025-04-16 15:42:13 +0100

    adding production environment

--- db/connection.js
+++ db/connection.js
@@ -4,13 +4,18 @@ const ENV = process.env.NODE_ENV || 'development'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-if(!process.env.PGDATABASE) {
-    throw new Error("PGDATABASE not configured")
+if(!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not configured")
 } else {
     console.log(`Connected to ${process.env.PGDATABASE}`)
 }
 
 const config = {};
+
+if (ENV === "production") {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 5;
+  }
 
 const db = new Pool(config);
 
